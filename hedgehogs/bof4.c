cbc123fd55c21088f2e3107cd9edea5c7132ddbf
20om3 20om3 <uta.suga@gmail.com>
2021-05-21 19:21:05 +0900

    hedgehog pwn section done in 2days

--- hedgehogs/bof4.c
+++ hedgehogs/bof4.c
@@ -1,10 +1,18 @@
+//gcc -m32 -no-pie -o bof4 bof4.c
+
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
+void vuln(){
     char msg[] = "Hello\n";
     char buf[32];
     write(1, msg, strlen(msg));
     read(0, buf, 128);
+    return;
+}
+
+int main(int argc, char *argv[]) {
+    vuln();	
     return 0;
+
 }
